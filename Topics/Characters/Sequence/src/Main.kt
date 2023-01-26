bab533b170c51da18f83e56fd925e433900e53f8
Ash2033 <Ashermance@gmail.com>
2023-01-25 21:36:36 -0500

    Updated character task 'sequence' code comments

--- Topics/Characters/Sequence/src/Main.kt
+++ Topics/Characters/Sequence/src/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
     // put your code here
-    val inpt = readln().first().code
+    val inpt = readln().first().code // uses the code attribute of characters to make it into an integer
     val chk = readln().first().code
     val chk2 = readln().first().code
-    inpt
+    inpt //initializes each input
     chk
     chk2
-    if ((inpt + 1 == chk) && (chk + 1 == chk2)) {
+    if ((inpt + 1 == chk) && (chk + 1 == chk2)) { //checks if the first input is the same as the 2nd plus 1 rung on the unicode ladder, then against the next as well, and if they're in line it prints true, if not it prints false
         println(true)
     }
         else {
